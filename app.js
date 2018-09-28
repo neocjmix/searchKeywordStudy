29aef9aaa120dd207a0da87aa93d160e44b399b5
frank.park <frank.park@kakaocorp.com>
2018-09-28 13:18:40 +0900

    naver scrapping

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import BigQuery from '@google-cloud/bigquery'
+import axios from 'axios'
+import $ from 'cheerio'
 
 const bigquery = new BigQuery({projectId: 'searchkeywordstudy'});
 const getFirst = data => data[0];
@@ -12,19 +14,49 @@ async function setupTable({datasetName, tableName}) {
         await table.setMetadata({
             name: 'Search Keyword Graph',
             description: 'A table for storing Search Keyword Graph.',
-            schema: 'name:string, value:integer'
+            schema: 'service:string, from:string, to:string, timestamp:timestamp'
         });
     }
     return table;
 }
 
 (async () => {
+
     const table = await setupTable({
         datasetName: 'search_keyword_study_data',
         tableName: 'search_keyword_graph'
     });
-    table.insert({
-        name: 'Motion Picture Institute of Michigan',
-        value: 1
-    })
+
+    const service = 'naver';
+    const keyword = "아이유";
+    const naverSearchUrl = 'https://search.naver.com/search.naver';
+    const userAgentString = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36';
+    const KeywordDeleteRegex = new RegExp(keyword,"g");
+
+    try{
+        const response = await axios.get(naverSearchUrl, {
+            params: {
+                query: keyword
+            },
+            headers: {
+                'user-agent' : userAgentString
+            },
+            timeout: 3000
+        });
+        const $html = $(response.data);
+        const $links = $html.find(".sp_keyword .lst_relate li a");
+
+        const linkedKeywords = $links.map((i, link) => $(link).text().replace(KeywordDeleteRegex,"").trim());
+        const insertList = Array.from(linkedKeywords)
+            .map(linkedKeyword => ({
+                service: service,
+                from: keyword,
+                to: linkedKeyword,
+                timestamp: bigquery.datetime(new Date().toISOString())
+            }));
+
+        await table.insert(insertList);
+    }catch(error){
+        console.error(JSON.stringify(error));
+    }
 })();
